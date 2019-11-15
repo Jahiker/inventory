0fcac15cd5751f563929c42b2c2cd0b155b61c93
Jahiker <rojasjahiker@gmail.com>
2019-11-15 13:11:07 -0400

    Segundo Commit

---
+++ conexion copy.php
@@ -0,0 +1,5 @@
+<?php  
+	$link=mysqli_connect("localhost","root","")or die('No se pudo conectar al servidor'.mysqli_error($link));
+	mysqli_select_db($link,"inventory");
+?>
+
--- conexion.php
+++ conexion.php
@@ -1,4 +1,5 @@
 <?php  
-	$link=mysqli_connect("localhost","root","")or die('No se pudo conectar al servidor'.mysqli_error($link));
-	mysqli_select_db($link,"inventory");
+	$link=mysqli_connect("remotemysql.com","zeXki1BvFa","uIxT353YQU")or die('No se pudo conectar al servidor'.mysqli_error($link));
+	mysqli_select_db($link,"zeXki1BvFa");
 ?>
+
